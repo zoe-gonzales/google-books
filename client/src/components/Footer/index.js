6dccc3c65c6aadb45a9f7e208945066406625453
zoe-gonzales <zoegonzales03@gmail.com>
2019-05-05 12:07:07 -0600

    footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <a 
+            href="https://www.google.com/intl/en/googlebooks/about/index.html" 
+            target="_blank"
+            rel="noopener noreferrer">
+            About Google Books</a>
+        </footer>
+    )
+}
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
+import Footer from '../components/Footer';
 import './style.css';
 
 class Search extends Component {
-
-
-
-
     render() {
         return (
             <div>
@@ -15,6 +12,7 @@ class Search extends Component {
                 <h1 className="sub-title">Search & Save Books</h1>
                 <SearchBar placeholder={"Search by keywords..."}/>
                 <SearchBar placeholder={"Search by author..."}/>
+                <Footer/>
             </div>
         )
     }
